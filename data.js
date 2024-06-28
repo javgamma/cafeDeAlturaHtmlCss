f0e1f4e7afd26d68667c8d26346b1d53f5d190b6
Javmir <167805975+javgamma@users.noreply.github.com>
2024-06-28 21:32:55 +0200

    New Pages

---
+++ data.js
@@ -0,0 +1,68 @@
+import costaRicaTarrazu from "./assets/img/costaRicaCoffe.png";
+import losNaranjos from "./assets/img/colombiaCofffee.png";
+import laosAmanecer from "./assets/img/laosCoffee.png";
+import etiopia from "./assets/img/etiopiaCoffee.png";
+import kenia from "./assets/img/keniaCooffe.png";
+import etiopiaSidamo from "./assets/img/etiopiaSidamoCoffee.png";
+import monteBello from "./assets/img/monteBelloCoffee.png";
+import colombiaLacasita from "./assets/img/laCasitaCoffee.png"
+
+
+const dataBaseCoffee = [
+    {
+        id: "1",
+        productName:"Costa Rica Tarrazú",
+        isAvailable: true,
+        priceProduct: 10,
+        img: costaRicaTarrazu
+    },
+    {
+        id: "2",
+        productName:"Colombia Los Naranjos",
+        isAvailable: true,
+        priceProduct: 10,
+        img:losNaranjos
+    },
+    {
+        id: "3",
+        productName:"Laos Amanecer",
+        isAvailable: true,
+        priceProduct: 10,
+        img: laosAmanecer
+    },
+    {
+        id: "4",
+        productName:"Etiopía Urgacheff",
+        isAvailable: true,
+        priceProduct: 10,
+        img: etiopia
+    },
+    {
+        id: "5",
+        productName:"Kenia Ndunduri",
+        isAvailable: true,
+        priceProduct: 10,
+        img: kenia
+    },
+    {
+        id: "6",
+        productName:"Etiopía Sidamo",
+        isAvailable: true,
+        priceProduct: 10,
+        img:e
+    },
+    {
+        id: "7",
+        productName:"Costa Rica Monte Bello",
+        isAvailable: true,
+        priceProduct: 10,
+        img:
+    },
+    {
+        id: "8",
+        productName:"Colombia La Casita",
+        isAvailable: true,
+        priceProduct: 10,
+        img:
+    }
+]
